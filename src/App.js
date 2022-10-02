9fd66326b383ca1693027bd3f13dab45a1567b55
Karinnaor <109036074+Karinnaor@users.noreply.github.com>
2022-10-02 12:32:44 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="KarinNaor-logo img-fluid" alt="logo"></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
