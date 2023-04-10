75dca9a92fa27c57a0d0178ba628212fd832ce3a
Karinnaor <109036074+Karinnaor@users.noreply.github.com>
2023-04-10 12:37:20 +0300

    fixed pexels authorization

--- src/components/Dictionary/apiUtils.js
+++ src/components/Dictionary/apiUtils.js
@@ -8,6 +8,6 @@ export function fetchDefinition(searchTerm) {
 export function fetchImages(searchTerm) {
   let pexelsApiKey = "563492ad6f91700001000001d6539663b196434eaa66983c341f78b5";
   let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${searchTerm}&per_page=9`;
-  let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+  let headers = { Authorization: pexelsApiKey };
   return axios.get(pexelsApiUrl, { headers: headers });
 }
