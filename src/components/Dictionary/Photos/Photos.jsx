140c4f891e7f79deaa56abef89ec05b851418f28
Karinnaor <109036074+Karinnaor@users.noreply.github.com>
2023-04-10 12:15:52 +0300

    moved photos folder around

--- src/components/Dictionary/Photos/Photos.jsx
+++ src/components/Dictionary/Photos/Photos.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+
 import "./Photos.css";
 
 export default function Photos(props) {
+  console.log(props.photos);
   if (props.photos) {
     return (
       <section className="Photos">
@@ -9,9 +11,14 @@ export default function Photos(props) {
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
-              <div className="col-4" key={index}>
+              <div className="col-4">
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img className="img-fluid" src={photo.src.landscape} alt="" />
+                  <img
+                    className="img-fluid"
+                    src={photo.src.landscape}
+                    alt=""
+                    key={index}
+                  />
                 </a>
               </div>
             );
